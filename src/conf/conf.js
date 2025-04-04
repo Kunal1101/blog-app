77a32464f81877b00f98888979587a66fbca6dbb
Kunal Mishra <kunalmishra8127@gmail.com>
2025-04-04 15:52:45 +0530

    blog app setup

---
+++ src/conf/conf.js
@@ -0,0 +1,7 @@
+const confUrl = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_IDL),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
